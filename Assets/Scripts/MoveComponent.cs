d57921b38c1966fdb0e7797101234efaece1d4e5
Superslownic <isuperslownic@gmail.com>
2021-02-11 21:22:51 +0700

    Added move component

---
+++ Assets/Scripts/MoveComponent.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace PingPong
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class MoveComponent : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+
+        public bool IsMoving { get; private set; }
+        public Vector2 Direction { get; private set; }
+
+        private Rigidbody2D _rigidbody;
+
+        public void Move(Vector2 direction)
+        {
+            IsMoving = true;
+            Direction = direction;
+        }
+
+        public void Stop()
+        {
+            IsMoving = false;
+        }
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            if (IsMoving == false) return;
+            
+            _rigidbody.velocity = Direction;
+        }
+    }
+}
