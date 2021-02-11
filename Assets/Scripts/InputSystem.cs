9d8d164c6bcbace0e718918dec69b828b312205a
Superslownic <isuperslownic@gmail.com>
2021-02-11 22:16:12 +0700

    Input system

---
+++ Assets/Scripts/InputSystem.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace PingPong
+{
+    public class InputSystem : MonoBehaviour, IPointerDownHandler, IDragHandler
+    {
+        public Vector3 TouchPosition { get; private set; }
+
+        private Camera _camera;
+        
+        void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+        {
+            CalculatePosition(eventData);
+        }
+
+        void IDragHandler.OnDrag(PointerEventData eventData)
+        {
+            CalculatePosition(eventData);
+        }
+
+        private void Awake()
+        {
+            _camera = Camera.main;
+        }
+
+        private void CalculatePosition(PointerEventData eventData)
+        {
+            TouchPosition = _camera.ScreenToWorldPoint(eventData.position);
+        }
+    }
+}
