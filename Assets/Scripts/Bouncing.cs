fd1e5f99aaaf1aefc87bf434e0628a3bca6d153d
Superslownic <isuperslownic@gmail.com>
2021-02-11 21:50:09 +0700

    Bouncing

---
+++ Assets/Scripts/Bouncing.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace PingPong
+{
+    [RequireComponent(typeof(PhysicsMovement))]
+    public class Bouncing : MonoBehaviour
+    {
+        private PhysicsMovement _physicsMovement;
+
+        private void Awake()
+        {
+            _physicsMovement = GetComponent<PhysicsMovement>();
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            Vector2 normal = collision.contacts[0].normal;
+            _physicsMovement.Move(Vector2.Reflect(_physicsMovement.Direction, normal));
+        }
+    }
+}
